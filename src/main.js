53e6b1bbf248e32f5a1f28adfd54d3746705ff56
lin_xi <1242838858@qq.com>
2019-08-31 20:06:52 +0800

    页面布局及样式

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import router from './router'
 import Element from 'element-ui'
 // 引入样式
 import 'element-ui/lib/theme-chalk/index.css'
+import './style/index.less' // 引入初始化样式 包括 margin padding li
 Vue.config.productionTip = false
 // 使用ui
 Vue.use(Element)
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,15 +1,50 @@
 <template>
-  <div>
-login
+  <div class="login">
+    <el-card class="box-card">
+      <div class="logo">
+        <img src="../../assets/img/logo_index.png" alt="黑马头条" />
+      </div>
+      <el-form style="margin-top:20px">
+        <el-form-item prop="mobile">
+          <el-input placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item prop="code">
+          <el-input placeholder="请输入密码" style="width:280px"></el-input>
+          <el-button style='float:right'>发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" style='width:100%'>登录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
-<style>
-
+<style lang='less' scoped>
+.login {
+  background-image: url("../../assets/img/login_bg.jpg");
+  width: 100%;
+  height: 100vh;
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .box-card {
+    width: 450px;
+    height: 350px;
+    .logo {
+      text-align: center;
+    }
+    img {
+      height: 45px;
+    }
+  }
+}
 </style>

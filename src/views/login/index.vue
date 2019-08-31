47d80ab0c8db77b59cc70e27149b20059dc7ec00
lin_xi <1242838858@qq.com>
2019-08-31 22:23:58 +0800

    数据绑定及手机号 验证码 是否同意的校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,19 +4,27 @@
       <div class="logo">
         <img src="../../assets/img/logo_index.png" alt="黑马头条" />
       </div>
-      <el-form style="margin-top:20px">
+      <!-- 表单组件 el-form表单容器 -->
+      <!-- 数据校验 首先要给el-form 一个model属性 表示数据对象 -->
+      <!-- res是为了选取loginForm绑定数据 -->
+      <el-form style="margin-top:20px" :model="loginForm" ref="loginForm" :rules="loginRules">
+        <!-- 表单项  prop绑定需要校验的字段名 但是 不写loginForm.mobile只写mobile -->
         <el-form-item prop="mobile">
-          <el-input placeholder="请输入手机号"></el-input>
+          <!-- 放置组件内容 -->
+          <!-- 绑定手机号 -->
+          <el-input placeholder="请输入手机号" v-model="loginForm.mobile"></el-input>
         </el-form-item>
         <el-form-item prop="code">
-          <el-input placeholder="请输入密码" style="width:280px"></el-input>
-          <el-button style='float:right'>发送验证码</el-button>
+          <!-- 绑定验证码 -->
+          <el-input v-model="loginForm.code" placeholder="请输入验证码" style="width:280px"></el-input>
+          <el-button style="float:right">发送验证码</el-button>
+        </el-form-item>
+        <el-form-item prop="check">
+          <!-- 绑定是否勾选选协议 -->
+          <el-checkbox v-model="loginForm.check">我已阅读并同意用户协议和隐私条款</el-checkbox>
         </el-form-item>
         <el-form-item>
-          <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" style='width:100%'>登录</el-button>
+          <el-button type="primary" style="width:100%">登录</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -24,10 +32,73 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    let validator = function (rule, value, callBack) {
+      if (value) {
+        callBack() // 如果value为true直接通过
+      } else {
+        callBack(new Error('您必须无条件同意被坑'))
+      }
+    }
+    return {
+      loginForm: {
+        mobile: '', // 手机号
+        code: '', // 验证码
+        check: false // 是否勾选协议
+      },
+      //   定义rules校验规则  表单是根据规则去校验  没有规则 就没有校验
+      // key(字段名):value(数组对象=> 多个 => 一个字段 可能有一个或者多个校验规则)
+      loginRules: {
+        mobile: [
+          {
+            required: true, // 意味着必填
+            message: '手机号不能为空' // 如果没有满足要求 就会提示message的内容
+          },
+          {
+            pattern: /^1[3456789]\d{9}/, // 正则表达式
+            message: '手机格式不正确'
+          }
+        ],
+        code: [
+          {
+            required: true,
+            message: '验证码不能为空'
+          },
+          {
+            pattern: /^\d{6}$/,
+            message: '验证码必须为6为数字'
+          }
+        ],
+        check: [
+          {
+            validator
+          }
+        ]
+      }
+    }
+  }
+  // methods: {
+  // login () {
+  //   // 通过el-form组件的validata方法 校验整个表单的数据
+  //   // 传入一个回调函数 isOK为true 说明所有的校验规则都成功了
+  //   // 如果false 说明有错误
+  //   this.$refs.loginForm.validator(isOK => {
+  //     if (isOK) {
+  //       // 请求
+  //       // axios 中 data中放置body参数 params是放置地址参数的
+  //     }
+  //   })
+  // }
+  // }
+}
 </script>
 
 <style lang='less' scoped>
+// 如果要用less/scss等预处理css语言的话 需要给一个lang属性 lang='less' lang='scss'
+// lang属性 是对但css进行语言指定
+// scoped 只针对当前的组件样式起作用
+// 100vh 就是占据当前的屏幕的100%
 .login {
   background-image: url("../../assets/img/login_bg.jpg");
   width: 100%;

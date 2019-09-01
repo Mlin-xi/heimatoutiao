75a41c7ef8a8ba653f9af8f900179dcc02e62059
lin_xi <1242838858@qq.com>
2019-09-01 22:41:06 +0800

    头部请求信息

--- src/components/home/layout-header.vue
+++ src/components/home/layout-header.vue
@@ -7,11 +7,11 @@
       <span>江苏传智播客教育科技股份有限公司</span>
     </el-col>
     <el-col :span="3" class="right">
-      <img class="head-img" src="../../assets/img/avatar.jpg" alt />
+      <img class="head-img" :src="userInfo.photo? userInfo.photo:defaultImg" alt />
       <el-dropdown trigger="click">
         <!-- 匿名插槽 -->
         <span class="el-dropdown-link">
-          姚鑫我爱你啊
+          {{userInfo.name}}
           <i class="el-icon-arrow-down el-icon--right"></i>
         </span>
         <el-dropdown-menu slot="dropdown">
@@ -25,7 +25,35 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      userInfo: {},
+      defaultImg: require('../../assets/img/avatar.jpg') // 转成base64 字符串
+    }
+  },
+  methods: {
+    // 获取用户数据
+    getUserInfo () {
+      let token = window.localStorage.getItem('user-token') // 获取token
+      this.$axios({
+        url: '/user/profile',
+        headers: { Authorization: `Bearer ${token}` }
+      })
+        .then(res => {
+          // console.log(res)
+          this.userInfo = res.data.data
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  },
+  created () {
+    // 钩子函数 一进页面就拉取数据
+    this.getUserInfo()
+  }
+}
 </script>
 
 <style lang='less' scoped>

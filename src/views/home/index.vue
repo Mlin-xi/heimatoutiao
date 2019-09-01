17335f0add8cc92c4443efef6dc60a8dfa46d146
lin_xi <1242838858@qq.com>
2019-09-01 19:48:21 +0800

    左侧导航菜单

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,15 +1,28 @@
 <template>
-<div>
-home
-</div>
+  <!-- 最外层容器 -->
+  <el-container>
+    <!-- 左侧导航 -->
+    <el-aside>
+      <lay-out></lay-out>
+    </el-aside>
+    <!-- 右侧 -->
+    <el-container>
+      <!-- <头部 -->
+      <el-header>头部</el-header>
+      <!-- 主区 -->
+      <el-main>主体</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
+import layOut from '../../components/home/layout-aside'
 export default {
-
+  components: {
+    'lay-out': layOut
+  }
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
 </style>

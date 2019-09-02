4bfe803cf7358287e4bc54632db30a299e80a954
lin_xi <1242838858@qq.com>
2019-09-02 11:20:57 +0800

    左侧导航路由配置

--- src/components/home/layout-aside.vue
+++ src/components/home/layout-aside.vue
@@ -8,8 +8,9 @@
       style="width:201px"
       text-color="#adafb5"
       active-text-color="#ffd04b"
+      router
     >
-      <el-menu-item index="2">
+      <el-menu-item index="/">
         <i class="el-icon-s-home"></i>
         <span slot="title">首页</span>
       </el-menu-item>
@@ -20,10 +21,10 @@
           <i class="el-icon-s-claim"></i>
           <span>内容管理</span>
         </template>
-        <el-menu-item>发布文章</el-menu-item>
-        <el-menu-item>内容列表</el-menu-item>
-        <el-menu-item>评论管理</el-menu-item>
-        <el-menu-item>素材管理</el-menu-item>
+        <el-menu-item index="/home/publish">发布文章</el-menu-item>
+        <el-menu-item index="/home/articles">内容列表</el-menu-item>
+        <el-menu-item index="/home/commit">评论管理</el-menu-item>
+        <el-menu-item index="material">素材管理</el-menu-item>
       </el-submenu>
       <el-submenu index="2">
         <!-- el-submenu定义了具名插槽 -->
@@ -31,13 +32,13 @@
           <i class="el-icon-s-custom"></i>
           <span>粉丝管理</span>
         </template>
-        <el-menu-item>图文数据</el-menu-item>
-        <el-menu-item>粉丝概况</el-menu-item>
-        <el-menu-item>粉丝列表</el-menu-item>
-        <el-menu-item>粉丝画像</el-menu-item>
+        <el-menu-item index="/home/fansdata">图文数据</el-menu-item>
+        <el-menu-item index="/home/fansinfo">粉丝概况</el-menu-item>
+        <el-menu-item index="/home/fanslist">粉丝列表</el-menu-item>
+        <el-menu-item index="/home/fansimg">粉丝画像</el-menu-item>
       </el-submenu>
-      <el-menu-item index="4">
-        <i class="el-icon-s-tools"></i>
+      <el-menu-item>
+        <i class="el-icon-s-tools" index="/home/account"></i>
         <span slot="title">账号信息</span>
       </el-menu-item>
     </el-menu>

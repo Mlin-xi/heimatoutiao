8754bd99e9c2844e153efb41bca92b72dee0f92e
lin_xi <1242838858@qq.com>
2019-09-05 22:00:08 +0800

    发布校验

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -3,28 +3,34 @@
     <bread-crumb slot="header">
       <template slot="title">发布文章</template>
     </bread-crumb>
-    <!-- 表单 model 数据对象 rules 绑定规则 prop用来校验的-->
-    <el-form style="margin-left:100px" label-width="100px">
-      <el-form-item label="标题">
-        <el-input style="width:400px"></el-input>
+    <!-- 表单 model 数据对象 rules 绑定规则 prop用来校验的 ref是校验全部的属性-->
+    <el-form
+      ref="publishForm"
+      :model="formData"
+      :rules="publishRules"
+      style="margin-left:100px"
+      label-width="100px"
+    >
+      <el-form-item label="标题" prop="title">
+        <el-input style="width:400px" v-model="formData.title"></el-input>
       </el-form-item>
-      <el-form-item label="内容">
-        <el-input type="textarea" :rows="4"></el-input>
+      <el-form-item label="内容" prop="content">
+        <el-input type="textarea" :rows="4" v-model="formData.content"></el-input>
       </el-form-item>
-      <el-form-item label="封面">
+      <el-form-item label="封面" v-model="formData.cover.type">
         <el-radio :label="1">单图</el-radio>
         <el-radio :label="3">三图</el-radio>
         <el-radio :label="0">无图</el-radio>
         <el-radio :label="-1">自动</el-radio>
       </el-form-item>
-      <el-form-item label="频道">
+      <el-form-item prop="channel_id" label="频道" v-model="formData.channel_id">
         <el-select>
           <!-- label(显示值)  value(真实值) -->
           <el-option v-for="item in channels" :key="item.id" :label="item.name" :value="item.id"></el-option>
         </el-select>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary">发表文章</el-button>
+        <el-button type="primary" @click="publish">发表文章</el-button>
         <el-button>存入草稿</el-button>
       </el-form-item>
     </el-form>
@@ -35,7 +41,38 @@
 export default {
   data () {
     return {
-      channels: []
+      channels: [],
+      formData: {
+        title: '',
+        content: '',
+        cover: {
+          // 封面类型 -1:自动，0-无图，1-1张，3-3张
+          type: 0,
+          images: []
+        },
+        channel_id: null
+      },
+      //   校验规则
+      publishRules: {
+        title: [
+          {
+            required: true,
+            message: '标题不能为空'
+          }
+        ],
+        content: [
+          {
+            required: true,
+            message: '内容不能为空'
+          }
+        ],
+        channel_id: [
+          {
+            required: true,
+            message: '频道不能为空'
+          }
+        ]
+      }
     }
   },
   methods: {
@@ -45,6 +82,13 @@ export default {
         url: '/channels'
       }).then(res => {
         this.channels = res.data.channels
+      })
+    },
+    // 发布文章
+    publish () {
+      this.$refs.publishForm.validate(function (isOk) {
+        if (isOk) {
+        }
       })
     }
   },

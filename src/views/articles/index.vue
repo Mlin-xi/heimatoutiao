b88d68f3c78e7567498358538a5d8a5b63089f07
lin_xi <1242838858@qq.com>
2019-09-05 16:20:55 +0800

    内容列表-页面结构

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -17,7 +17,7 @@
       </el-form-item>
       <el-form-item label="频道列表:">
         <el-select placeholder="请选择活动区域">
-          <el-option label="区域一"></el-option>
+          <el-option label="区域一" value="1"></el-option>
         </el-select>
       </el-form-item>
       <el-form-item label="时间选择:">
@@ -29,12 +29,87 @@
         ></el-date-picker>
       </el-form-item>
     </el-form>
+    <!-- 页面结构 -->
+    <div class="total-info">共找到3456条符合条件的内容</div>
+    <div class="article-list">
+      <!-- 循环 -->
+      <div v-for="(item,index) in list" :key="index" class="article-item">
+        <!-- 左侧 -->
+        <div class="left">
+          <img src="../../assets/img/404.png" alt />
+          <div class="info">
+            <span class="title">有内鬼</span>
+            <el-tag style="width:60px">已发布</el-tag>
+            <span class="date">2019-09-03 08:15:26</span>
+          </div>
+        </div>
+        <!-- 右侧 -->
+        <div class="right">
+          <span>
+            <i class="el-icon-edit"></i>修改
+          </span>
+          <span>
+            <i class="el-icon-delete"></i>删除
+          </span>
+        </div>
+      </div>
+    </div>
   </el-card>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      list: [1, 2, 3] // 定义一个空数组
+    }
+  }
+}
 </script>
 
-<style>
+<style lang='less' scoped>
+.total-info {
+  border-bottom: 1px dashed #ccc;
+  padding: 15px 0;
+  color: #313745;
+  font-size: 14px;
+}
+.article-list {
+  .article-item {
+    display: flex;
+    justify-content: space-between;
+    padding: 15px 0;
+    border-bottom: 1px solid #ccc;
+    .left {
+      display: flex;
+      img {
+        width: 180px;
+        height: 100px;
+        border-radius: 4px;
+      }
+      .info {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        margin-left: 10px;
+        .date {
+          color: #999;
+          font-size: 12px;
+        }
+        .title {
+          font-size: 14px;
+          color: #333;
+        }
+      }
+    }
+    .right {
+      font-size: 12px;
+      span {
+        margin: 0 8px;
+        cursor: pointer;
+        color: #333;
+      }
+    }
+  }
+}
 </style>

a9f7e6b808a94e64b3b1e22a4e9a64b5eb340849
lin_xi <1242838858@qq.com>
2019-09-05 21:41:43 +0800

    页面结构-简单实现-频道数据加载

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -4,14 +4,54 @@
       <template slot="title">发布文章</template>
     </bread-crumb>
     <!-- 表单 model 数据对象 rules 绑定规则 prop用来校验的-->
-    <el-form>
-      <el-form-item label="标题"></el-form-item>
+    <el-form style="margin-left:100px" label-width="100px">
+      <el-form-item label="标题">
+        <el-input style="width:400px"></el-input>
+      </el-form-item>
+      <el-form-item label="内容">
+        <el-input type="textarea" :rows="4"></el-input>
+      </el-form-item>
+      <el-form-item label="封面">
+        <el-radio :label="1">单图</el-radio>
+        <el-radio :label="3">三图</el-radio>
+        <el-radio :label="0">无图</el-radio>
+        <el-radio :label="-1">自动</el-radio>
+      </el-form-item>
+      <el-form-item label="频道">
+        <el-select>
+          <!-- label(显示值)  value(真实值) -->
+          <el-option v-for="item in channels" :key="item.id" :label="item.name" :value="item.id"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary">发表文章</el-button>
+        <el-button>存入草稿</el-button>
+      </el-form-item>
     </el-form>
   </el-card>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      channels: []
+    }
+  },
+  methods: {
+    //   获取数据
+    getChannels () {
+      this.$axios({
+        url: '/channels'
+      }).then(res => {
+        this.channels = res.data.channels
+      })
+    }
+  },
+  created () {
+    this.getChannels()
+  }
+}
 </script>
 
 <style>
